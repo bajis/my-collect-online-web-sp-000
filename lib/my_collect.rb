c0078cbb7fef4b93ce51f0e9f18b57576b5ee0ef
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-05-01 05:11:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collection(collection)
   counter = 0
   collection = []
   while counter <collection.length
-    list << yiel(collection[counter])
+    collection << yiel(collection[counter])
     counter += 1
   end
-  list
+  collection
 end
